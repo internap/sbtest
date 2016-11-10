e0340d078cd10498e7978dde6f519a6ca6db04e1
Martin Roy <mroy@internap.com>
2016-11-10 17:49:42 -0500

    Add string support to assert equals

--- src/asserts.sh
+++ src/asserts.sh
@@ -11,11 +11,11 @@ assert() {
     local directive=$2
     local expected=$3
 
-    _assert_${directive} ${actual} ${expected}
+    _assert_${directive} "${actual}" "${expected}"
 }
 
 _assert_equals() {
-    test ${1} -eq ${2} || fail "Expected <$2>\nGot:      <$1>"
+    test "${1}" == "${2}" || fail "Expected <$2>\nGot:      <$1>"
 }
 
 _assert_succeeded() {
